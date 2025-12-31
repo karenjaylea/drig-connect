0eaf2f90a3d836b1f0386854d400ed65c2524324
karenjaylea <karenjaylea@gmail.com>
2025-12-31 09:37:02 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'  // keep using your existing CSS
 
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+root.render(<App />)
